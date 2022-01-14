972ea63bd30da43d959a037ce21063b8197ddc19
98912 <98912@DESKTOP-A33184V>
2022-01-14 10:09:52 +0330

    Add project files.

---
+++ SearchRecord/DisplayRecords.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DisplayRecords.aspx.cs" Inherits="SearchRecord.DisplayRecords" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" >
+        <div >
+            <center>
+                <h1>جستجو</h1>
+                 <hr />
+            <asp:TextBox ID="TextSearch" runat="server"></asp:TextBox>
+            <asp:Button ID="ButSearch" runat="server" Text="جستجو" OnClick="ButSearch_Click" />
+            <hr />
+            <asp:GridView ID="GridView1" runat="server" ShowHeaderWhenEmpty="true" EmptyDataText="رکورد یافت نشد">
+
+            </asp:GridView>
+                </center>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SearchRecord/DisplayRecords.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace SearchRecord
+{
+    public partial class DisplayRecords : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            sqlconn.Open();
+            SqlCommand sqlcomm = new SqlCommand();
+            string sqlquery = "select * from [dbo].[join8] where Yymm like '%'+@Yymm+'%'";
+            sqlcomm.CommandText = sqlquery;
+            sqlcomm.Connection = sqlconn;
+            sqlcomm.Parameters.AddWithValue("Yymm",TextSearch.Text);
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(sqlcomm);
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+           
+ 
+        }
+
+        protected void ButSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
